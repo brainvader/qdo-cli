2abd66fce92ac50a8518c6a097207d12cb8f007e
Brainvader <brainvade@gmail.com>
2021-09-18 18:37:56 +0900

    Create tera instance lazily

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use once_cell::sync::Lazy;
+use tera::{Context, Result, Tera};
+
+static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
+    let mut tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    tera.autoescape_on(vec!["html", ".sql"]);
+    tera
+});
+
 fn main() {
     println!("Hello, world!");
 }
