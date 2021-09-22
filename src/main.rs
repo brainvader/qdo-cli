abc4fef1241f0997f67615c30a30d2773b101be0
Brainvader <brainvade@gmail.com>
2021-09-22 20:31:28 +0900

    Add struct for command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::fs;
+use std::path::PathBuf;
 
 use once_cell::sync::Lazy;
+use structopt::StructOpt;
 use tera::{Context, Result, Tera};
 
+// templates folder
+const TEMPLATES_DIR: &str = "templates/**/*";
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "qdo", about = "quiz generator")]
+struct Opt {
+    /// quiuz title
+    #[structopt(short = "t", long = "title")]
+    title: String,
+
+    /// template file in templates folder
+    #[structopt(parse(from_os_str), short = "f", long = "file")]
+    template: PathBuf,
+
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str), short = "o", long = "out")]
+    output: Option<PathBuf>,
+}
+
 static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
-    let mut tera = match Tera::new("templates/**/*") {
+    let mut tera = match Tera::new(TEMPLATES_DIR) {
         Ok(t) => t,
         Err(e) => {
             println!("Parsing error(s): {}", e);
@@ -17,6 +38,9 @@ static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
 
 fn main() -> Result<()> {
     let mut context = Context::new();
+
+    let args = Opt::from_args();
+    println!("{:?}", args);
     let title = "Sample Quiz";
     context.insert("title", &title);
 
