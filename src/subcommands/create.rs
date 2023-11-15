02457658981c2a26458017751fe3068f724ee562
brainvader <brainvader@gmail.com>
2023-11-15 13:10:22 +0000

    feat: Construct path to quiz file

--- src/subcommands/create.rs
+++ src/subcommands/create.rs
@@ -1,7 +1,6 @@
 use crate::utils;
 use anyhow::{Context, Result};
 use clap::Parser;
-use uuid::timestamp;
 
 #[derive(Parser)]
 pub struct Args {
@@ -13,19 +12,27 @@ pub struct Args {
 pub fn create_quiz() -> Result<()> {
     let uuid = utils::quiz_uuid();
     let (year, month, day, time) = utils::gen_timestamp();
-    println!(
-        "quiz {} is generated at {}-{}-{}-{}",
-        uuid, year, month, day, time
-    );
+    let qdo_path = utils::get_qdo_path().with_context(|| "Failed to full path to qdo dir")?;
+    let mut quiz_path = qdo_path
+        .join(year.to_string())
+        .join(month.to_string())
+        .join(day.to_string())
+        .join(uuid.to_string());
+    quiz_path.set_extension("html");
+    println!("quiz: {:?}", quiz_path.display());
     Ok(())
 }
 
 pub fn dry_run() -> Result<()> {
     let uuid = utils::quiz_uuid();
     let (year, month, day, time) = utils::gen_timestamp();
-    println!(
-        "quiz {} is generated at {}-{}-{}-{}",
-        uuid, year, month, day, time
-    );
+    let qdo_path = utils::get_qdo_path().with_context(|| "Failed to full path to qdo dir")?;
+    let mut quiz_path = qdo_path
+        .join(year.to_string())
+        .join(month.to_string())
+        .join(day.to_string())
+        .join(uuid.to_string());
+    quiz_path.set_extension("html");
+    println!("quiz: {:?}", quiz_path.display());
     Ok(())
 }
