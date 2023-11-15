11a148b86207a93190b1850c412ef46669b9c8bb
brainvader <brainvader@gmail.com>
2023-11-15 12:58:39 +0000

    refactor: Move some functions

--- src/subcommands/init.rs
+++ src/subcommands/init.rs
@@ -1,7 +1,6 @@
+use crate::utils;
 use anyhow::{Context, Result};
 use clap::Parser;
-use std::path::PathBuf;
-use std::{env, fs};
 
 #[derive(Parser)]
 pub struct InitArgs {
@@ -10,36 +9,14 @@ pub struct InitArgs {
     pub dry_run: bool,
 }
 
-fn get_home_dir() -> Result<String> {
-    let home_dir =
-        env::var("HOME").with_context(|| "Failed to retrieve HOME environment variable")?;
-    Ok(home_dir)
-}
-
-fn gen_qdo_dir(home_dir: &str) -> Result<()> {
-    let qdo_dir = PathBuf::from(home_dir).join("qdo");
-
-    // Check if the qdo directory exists
-    if !qdo_dir.exists() {
-        // Create the directory if it does not exist
-        fs::create_dir(&qdo_dir)?;
-        println!("ディレクトリを作成しました: {:?}", qdo_dir);
-    } else {
-        // Inform if the directory already exists
-        println!("ディレクトリは既に存在します: {:?}", qdo_dir);
-    }
-
-    Ok(())
-}
-
 pub fn initialize_project() -> Result<()> {
-    let home_dir = get_home_dir().with_context(|| "Failed to obtain the home directory")?;
-    gen_qdo_dir(&home_dir);
+    let home_dir = utils::get_home_dir().with_context(|| "Failed to obtain the home directory")?;
+    utils::gen_qdo_dir(&home_dir);
     Ok(())
 }
 
 pub fn dry_run() -> Result<()> {
-    let home_dir = get_home_dir().with_context(|| "Failed to obtain the home directory")?;
+    let home_dir = utils::get_home_dir().with_context(|| "Failed to obtain the home directory")?;
     println!("Create {}/qdo", home_dir);
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,38 @@
+use anyhow::{Context, Result};
 use chrono::{offset::Utc, DateTime, Datelike};
 use chrono_tz::Asia::Tokyo;
 use chrono_tz::Tz;
+use std::path::PathBuf;
+use std::{env, fs};
 use uuid::Uuid;
+
+pub fn get_home_dir() -> Result<String> {
+    let home_dir =
+        env::var("HOME").with_context(|| "Failed to retrieve HOME environment variable")?;
+    Ok(home_dir)
+}
+
+pub fn get_qdo_path() -> Result<PathBuf> {
+    let home_dir = get_home_dir()?;
+    let qdo_dir = PathBuf::from(home_dir).join("qdo");
+    Ok(qdo_dir)
+}
+
+pub fn gen_qdo_dir(home_dir: &str) -> Result<()> {
+    let qdo_dir = get_qdo_path().with_context(|| "Failed to generate a full path to qdo")?;
+
+    // Check if the qdo directory exists
+    if !qdo_dir.exists() {
+        // Create the directory if it does not exist
+        fs::create_dir(&qdo_dir)?;
+        println!("ディレクトリを作成しました: {:?}", qdo_dir);
+    } else {
+        // Inform if the directory already exists
+        println!("ディレクトリは既に存在します: {:?}", qdo_dir);
+    }
+
+    Ok(())
+}
 
 pub fn quiz_uuid() -> Uuid {
     Uuid::new_v4()
