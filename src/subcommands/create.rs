f06b7ec6c2a5cbc5ac8f3817ae496740b57402b1
brainvader <brainvader@gmail.com>
2023-11-17 12:44:35 +0000

    refactor: Add get_quiz_path function

--- src/subcommands/create.rs
+++ src/subcommands/create.rs
@@ -10,29 +10,13 @@ pub struct Args {
 }
 
 pub fn create_quiz() -> Result<()> {
-    let uuid = utils::quiz_uuid();
-    let (year, month, day, time) = utils::gen_timestamp();
-    let qdo_path = utils::get_qdo_path().with_context(|| "Failed to full path to qdo dir")?;
-    let mut quiz_path = qdo_path
-        .join(year.to_string())
-        .join(month.to_string())
-        .join(day.to_string())
-        .join(uuid.to_string());
-    quiz_path.set_extension("html");
+    let quiz_path = utils::get_quiz_path()?;
     println!("quiz: {:?}", quiz_path.display());
     Ok(())
 }
 
 pub fn dry_run() -> Result<()> {
-    let uuid = utils::quiz_uuid();
-    let (year, month, day, time) = utils::gen_timestamp();
-    let qdo_path = utils::get_qdo_path().with_context(|| "Failed to full path to qdo dir")?;
-    let mut quiz_path = qdo_path
-        .join(year.to_string())
-        .join(month.to_string())
-        .join(day.to_string())
-        .join(uuid.to_string());
-    quiz_path.set_extension("html");
-    println!("quiz: {:?}", quiz_path.display());
+    let quiz_path = utils::get_quiz_path()?;
+    println!("Dry run: quiz path would be {:?}", quiz_path.display());
     Ok(())
 }
