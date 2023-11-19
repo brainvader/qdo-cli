9757af7ff7cae8435c32abec89a8d9d18460ea19
brainvader <brainvader@gmail.com>
2023-11-19 04:01:56 +0000

    refactor: Add error handling

--- src/subcommands/init.rs
+++ src/subcommands/init.rs
@@ -11,7 +11,7 @@ pub struct InitArgs {
 
 pub fn initialize_project() -> Result<()> {
     let home_dir = utils::get_home_dir().with_context(|| "Failed to obtain the home directory")?;
-    utils::gen_qdo_dir(&home_dir);
+    utils::gen_qdo_dir(&home_dir).with_context(|| "Failed to create a qdo directory")?;
     Ok(())
 }
 
